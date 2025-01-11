5dd1d3fe54ea1e8cfd10b6f21077e7d414770b1d
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2025-01-11 18:12:39 +0530

    Update pyCharm.sh

--- pyCharm.sh
+++ pyCharm.sh
@@ -1,69 +1,81 @@
 #!/bin/bash
 
-# Define functions for installation and uninstallation
+# Function to print colorful text
+print_color() {
+    case $1 in
+        "green") echo -e "\e[32m$2\e[0m" ;;
+        "yellow") echo -e "\e[33m$2\e[0m" ;;
+        "red") echo -e "\e[31m$2\e[0m" ;;
+        "blue") echo -e "\e[34m$2\e[0m" ;;
+        "bold") echo -e "\e[1m$2\e[0m" ;;
+        "underline") echo -e "\e[4m$2\e[0m" ;;
+        *) echo "$2" ;;
+    esac
+}
+
+# Function to install PyCharm
 install_pycharm() {
-    echo "------------------------------------------------------------"
-    echo "Installing PyCharm"
-    echo "------------------------------------------------------------"
+    print_color "blue" "------------------------------------------------------------"
+    print_color "blue" "Installing PyCharm"
+    print_color "blue" "------------------------------------------------------------"
     
     # Check architecture
     ARCH=$(uname -m)
     
     if [[ "$ARCH" == "x86_64" ]]; then
         URL="https://download.jetbrains.com/python/pycharm-community-2023.1.4.tar.gz"
-    elif [[ "$ARCH" == "arm64" ]]; then
-        URL="https://download.jetbrains.com/python/pycharm-community-2023.1.4.tar.gz"
-    elif [[ "$ARCH" == "aarch64" ]]; then
+    elif [[ "$ARCH" == "arm64" || "$ARCH" == "aarch64" ]]; then
         URL="https://download.jetbrains.com/python/pycharm-community-2023.1.4.tar.gz"
     else
-        echo "Unsupported architecture for installation! Exiting..."
+        print_color "red" "Unsupported architecture for installation! Exiting..."
         exit 1
     fi
 
     # Download PyCharm
-    echo "Downloading PyCharm..."
+    print_color "yellow" "Downloading PyCharm..."
     wget $URL -O pycharm.tar.gz
 
     if [[ $? -ne 0 ]]; then
-        echo "Download failed! Exiting..."
+        print_color "red" "Download failed! Exiting..."
         exit 1
     fi
 
     # Extracting the tar.gz file with a progress bar
-    echo "Extracting PyCharm..."
-    tar -xzf pycharm.tar.gz -C /opt/
-    
+    print_color "yellow" "Extracting PyCharm..."
+    tar -xzf pycharm.tar.gz -C /opt/ &> /dev/null
+
     # Create symlink for easy access
-    echo "Creating Symlink for PyCharm"
+    print_color "yellow" "Creating Symlink for PyCharm"
     sudo ln -s /opt/pycharm-community-*/bin/pycharm.sh /usr/local/bin/pycharm
-    
+
     # Clean up
     rm -f pycharm.tar.gz
 
-    echo "PyCharm has been installed successfully!"
+    print_color "green" "PyCharm has been installed successfully!"
 }
 
+# Function to uninstall PyCharm
 uninstall_pycharm() {
-    echo "------------------------------------------------------------"
-    echo "Uninstalling PyCharm"
-    echo "------------------------------------------------------------"
+    print_color "blue" "------------------------------------------------------------"
+    print_color "blue" "Uninstalling PyCharm"
+    print_color "blue" "------------------------------------------------------------"
 
     # Check if PyCharm is installed
     if [[ ! -f "/usr/local/bin/pycharm" ]]; then
-        echo "PyCharm is not installed on your system."
+        print_color "yellow" "PyCharm is not installed on your system."
         return
     fi
 
     # Remove symlink
-    echo "Removing symlink..."
+    print_color "yellow" "Removing symlink..."
     sudo rm /usr/local/bin/pycharm
 
     # Remove PyCharm installation directory
-    echo "Removing PyCharm installation directory..."
+    print_color "yellow" "Removing PyCharm installation directory..."
     sudo rm -rf /opt/pycharm-community-*
 
     # Remove configuration files
-    echo "Removing PyCharm configuration files..."
+    print_color "yellow" "Removing PyCharm configuration files..."
     rm -rf ~/.config/JetBrains/PyCharm*
     rm -rf ~/.local/share/JetBrains/PyCharm*
     rm -rf ~/.cache/JetBrains/PyCharm*
@@ -74,21 +86,21 @@ uninstall_pycharm() {
     # Remove any other residual files
     find ~ -name "*pycharm*" -exec rm -rf {} \;
 
-    echo "PyCharm has been uninstalled successfully!"
+    print_color "green" "PyCharm has been uninstalled successfully!"
 }
 
 # Main script loop
 while true; do
     clear
-    echo "############################################################"
-    echo "#                   PyCharm Installer                      #"
-    echo "#                 Author: MaheshTechnicals                 #"
-    echo "############################################################"
+    print_color "blue" "############################################################"
+    print_color "blue" "#                   PyCharm Installer                      #"
+    print_color "blue" "#                 Author: MaheshTechnicals                 #"
+    print_color "blue" "############################################################"
     echo
-    echo "Please choose an option:"
-    echo "1) Install PyCharm"
-    echo "2) Uninstall PyCharm"
-    echo "3) Exit"
+    print_color "yellow" "Please choose an option:"
+    print_color "yellow" "1) Install PyCharm"
+    print_color "yellow" "2) Uninstall PyCharm"
+    print_color "yellow" "3) Exit"
     read -p "Enter your choice (1/2/3): " choice
     
     case $choice in
@@ -99,11 +111,11 @@ while true; do
             uninstall_pycharm
             ;;
         3)
-            echo "Exiting..."
+            print_color "green" "Exiting..."
             exit 0
             ;;
         *)
-            echo "Invalid choice. Please select 1, 2, or 3."
+            print_color "red" "Invalid choice. Please select 1, 2, or 3."
             ;;
     esac
     read -p "Press any key to continue..."
