acb09979c297ed54c926898c3e4f49798d1cadfd
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2025-01-11 18:31:18 +0530

    Update pyCharm.sh

--- pyCharm.sh
+++ pyCharm.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# Function to print colorful text
+# Function to print colorful output
 print_color() {
     case $1 in
-        "green") echo -e "\e[32m$2\e[0m" ;;
-        "yellow") echo -e "\e[33m$2\e[0m" ;;
-        "red") echo -e "\e[31m$2\e[0m" ;;
-        "blue") echo -e "\e[34m$2\e[0m" ;;
-        "bold") echo -e "\e[1m$2\e[0m" ;;
-        "underline") echo -e "\e[4m$2\e[0m" ;;
-        *) echo "$2" ;;
+        "red")
+            echo -e "\e[31m$2\e[0m"
+            ;;
+        "green")
+            echo -e "\e[32m$2\e[0m"
+            ;;
+        "yellow")
+            echo -e "\e[33m$2\e[0m"
+            ;;
+        "blue")
+            echo -e "\e[34m$2\e[0m"
+            ;;
+        *)
+            echo "$2"
+            ;;
     esac
 }
 
-# Function to install PyCharm
+# Function to check if PyCharm is installed
+is_pycharm_installed() {
+    if [ -d "/opt/pycharm" ] || [ -d "/opt/pycharm-community-2023.1.4" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Install PyCharm
 install_pycharm() {
     print_color "blue" "------------------------------------------------------------"
     print_color "blue" "Installing PyCharm"
@@ -60,85 +77,61 @@ install_pycharm() {
     print_color "green" "PyCharm has been installed successfully!"
 }
 
-# Function to uninstall PyCharm
+# Uninstall PyCharm
 uninstall_pycharm() {
     print_color "blue" "------------------------------------------------------------"
     print_color "blue" "Uninstalling PyCharm"
     print_color "blue" "------------------------------------------------------------"
+    
+    if is_pycharm_installed; then
+        print_color "yellow" "Removing PyCharm directories..."
+        sudo rm -rf /opt/pycharm*
+        
+        print_color "yellow" "Removing PyCharm symlink..."
+        sudo rm -f /usr/local/bin/pycharm
 
-    # Check if PyCharm is installed
-    if [[ ! -f "/usr/local/bin/pycharm" ]]; then
-        print_color "yellow" "PyCharm is not installed on your system."
-        return
+        print_color "green" "PyCharm has been uninstalled successfully!"
+    else
+        print_color "red" "PyCharm is not installed on your system."
     fi
-
-    # Remove the symlink
-    print_color "yellow" "Removing symlink..."
-    sudo rm /usr/local/bin/pycharm
-
-    # Remove PyCharm installation directory
-    print_color "yellow" "Removing PyCharm installation directory..."
-    sudo rm -rf /opt/pycharm-community-*
-
-    # Remove PyCharm directory if it exists
-    if [[ -d "/opt/pycharm" ]]; then
-        print_color "yellow" "Removing /opt/pycharm directory..."
-        sudo rm -rf /opt/pycharm
-    fi
-
-    # Check for any remaining PyCharm directories in /opt/
-    print_color "yellow" "Checking for any remaining PyCharm directories in /opt/..."
-    for dir in /opt/pycharm*; do
-        if [[ -d "$dir" ]]; then
-            print_color "yellow" "Removing remaining directory: $dir"
-            sudo rm -rf "$dir"
-        fi
-    done
-
-    # Remove configuration files
-    print_color "yellow" "Removing PyCharm configuration files..."
-    rm -rf ~/.config/JetBrains/PyCharm*
-    rm -rf ~/.local/share/JetBrains/PyCharm*
-    rm -rf ~/.cache/JetBrains/PyCharm*
-
-    # Remove desktop entries
-    rm -f ~/.local/share/applications/pycharm.desktop
-
-    # Remove any other residual files
-    find ~ -name "*pycharm*" -exec rm -rf {} \;
-
-    print_color "green" "PyCharm has been uninstalled successfully!"
 }
 
-# Main script loop
+# Main menu
 while true; do
-    clear
     print_color "blue" "############################################################"
-    print_color "blue" "#                   PyCharm Installer                      #"
-    print_color "blue" "#                 Author: MaheshTechnicals                 #"
+    print_color "blue" "#                   PyCharm Installer                    #"
+    print_color "blue" "#                 Author: MaheshTechnicals               #"
     print_color "blue" "############################################################"
-    echo
+
     print_color "yellow" "Please choose an option:"
     print_color "yellow" "1) Install PyCharm"
     print_color "yellow" "2) Uninstall PyCharm"
     print_color "yellow" "3) Exit"
+
     read -p "Enter your choice (1/2/3): " choice
-    
+
     case $choice in
         1)
+            # Check if PyCharm is installed, if yes, uninstall it first
+            if is_pycharm_installed; then
+                print_color "yellow" "PyCharm is already installed. Uninstalling first..."
+                uninstall_pycharm
+            fi
             install_pycharm
             ;;
         2)
             uninstall_pycharm
             ;;
         3)
-            print_color "green" "Exiting..."
-            exit 0
+            print_color "blue" "Exiting..."
+            break
             ;;
         *)
-            print_color "red" "Invalid choice. Please select 1, 2, or 3."
+            print_color "red" "Invalid choice! Please choose a valid option."
             ;;
     esac
-    read -p "Press any key to continue..."
+
+    read -p "Press any key to continue..." -n1 -s
+    echo ""
 done
 
